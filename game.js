ee5f11cf1551eb3e74438b3fc2e9bd141b496d71
Realharambe <dogeduck2.0@gmail.com>
2023-05-11 08:14:47 -0700

    Major update (0.4)
    In V0.4 I added a price Multiplier after you buy any tier, once you buy one its price will be multiplied by 2, im most likely going to lower it later, I also started working on a quest system currently it doesnt work since something broke

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 // Define variables
-let numPapers = 0;
-let numLawyers = 0;
+let numPapers = 0; // How much papers you have when you start
+let numLawyers = 0; // How much lawyers you have when you start
 let numLawyersTier2 = 0; // Added this variable
-let lawyerCost = 10;
-let lawyerTier2Cost = 80;
-let papersPerSecond = 0;
+let lawyerCost = 10; // Tier 1 cost
+let lawyerTier2Cost = 2500; // The cost of tier 2
+let papersPerSecond = 0; // How much you make per second
+let lawyerCostMultiplier = 2; // Added this variable
 
 // Define functions
 function updatePapers(num) {
@@ -25,13 +26,15 @@ function updateLawyersTier2(num) { // Added this function
 function updatePPS() {
   papersPerSecond = numLawyers + numLawyersTier2 * 4;
   document.getElementById("papers-per-second-1").innerHTML = numLawyers;
-  document.getElementById("papers-per-second-2").innerHTML = numLawyersTier2 * 4;
+  document.getElementById("papers-per-second-2").innerHTML = numLawyersTier2 * 25;
 }
 
 function buyLawyer() {
   if (numPapers >= lawyerCost) {
     updatePapers(-lawyerCost);
     updateLawyers(1);
+    lawyerCost *= 2; // Increase the cost
+    document.getElementById("lawyer-1-button").innerHTML = "Buy Lawyer Tier 1 - " + lawyerCost + " Papers"; // Update the button text
     updatePPS();
   }
 }
@@ -41,8 +44,10 @@ function buyLawyerTier2() {
     updatePapers(-lawyerTier2Cost);
     updateLawyersTier2(1); // Updated to use updateLawyersTier2()
     setInterval(function() {
-      updatePapers(4);
-    }, 500);
+      updatePapers(25);
+    }, Math.floor(Math.random() * 5 + 4) * 1000); // Generates a random number between 4-8 seconds and multiplies it by 1000 to convert to milliseconds
+    lawyerTier2Cost *= 1; // Update the cost of Lawyer Tier 2 to be 3 times the previous cost
+    document.getElementById("lawyer-2-button").innerHTML = "Buy Lawyer Tier 2 - " + lawyerTier2Cost + " Papers"; // Update the button text to display the new cost
     updatePPS();
   }
 }
@@ -64,3 +69,17 @@ document.getElementById("lawyer-2-button").addEventListener("click", buyLawyerTi
 
 // Call function to generate papers for tier 1 lawyers
 generatePapersPerSecond();
+
+//AutoClicker detection
+
+let clickCooldown = false;
+
+function onClick() {
+  if (!clickCooldown) {
+    // Perform click action here
+    clickCooldown = true;
+    setTimeout(function() {
+      clickCooldown = false;
+    }, 10000); // Set cooldown time to 10 second (10000 milliseconds)
+  }
+}
